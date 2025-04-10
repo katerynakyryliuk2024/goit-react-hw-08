7c3bf30de05eb81b7b3d4b0362d2c07f27899413
Kateryna Kyryliuk <katerynaerdek@gmail.com>
2025-04-10 18:23:56 +0300

    update auth/operations

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,11 +1,27 @@
 import css from "./RegistrationForm.module.css";
 import { Formik, Form, Field } from "formik";
+import { useDispatch } from "react-redux";
+import * as Yup from "yup";
+import { register } from "../../redux/auth/operations";
 
-const handleSubmit = (values, actions) => {
-  actions.resetForm();
-};
+const UserSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(3, "Must be min 3 chars")
+    .max(50, "Must be max 50 chars")
+    .required("This field is required"),
+  email: Yup.string().email("Invalid email").required("Required"),
+  password: Yup.string()
+    .min(8, "must be min 8 chars")
+    .required("This field is required"),
+});
 
 export default function RegistrationForm() {
+  const dispatch = useDispatch();
+
+  const handleSubmit = (values, actions) => {
+    actions.resetForm();
+    dispatch(register(values));
+  };
   return (
     <Formik
       initialValues={{
@@ -14,6 +30,7 @@ export default function RegistrationForm() {
         password: "",
       }}
       onSubmit={handleSubmit}
+      validationSchema={UserSchema}
     >
       <Form className={css.form} autoComplete="off">
         <label className={css.label}>
--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,3 +1,15 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-export const register = createAsyncThunk("auth/register", async () => {});
+export const register = createAsyncThunk(
+  "auth/register",
+  async (credentials, thunkAPI) => {
+    try {
+      const response = await axios.post("/users/signup", credentials);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+//snow@gmail.com
